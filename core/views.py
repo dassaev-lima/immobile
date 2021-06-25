0fe35632b69debde286f75347f8e9e3360254ee0
dassaev-lima <dassaevnascimento@gmail.com>
2021-06-25 00:26:35 -0300

    criando estrutura padrão das páginas e testando listagem de imóveis

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.models import Imovel
 
 # Create your views here.
+
+#view que lista os imóveis
+def lista_imoveis(request):
+    lista_de_imoveis = Imovel.objects.all()
+    dados = {"imoveis":lista_de_imoveis}
+    return render(request,'imoveis.html',dados)
